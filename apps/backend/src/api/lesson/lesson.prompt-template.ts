93b384bf3a47e45f52fa1719f94b7aa54a062097
robinsadeghpour <r.sadegh.f@gmail.com>
2023-07-16 11:22:00 +0200

    feat: lesson prompt template update

--- apps/backend/src/api/lesson/lesson.prompt-template.ts
+++ apps/backend/src/api/lesson/lesson.prompt-template.ts
@@ -3,19 +3,13 @@ import { LessonConfig, PromptTemplate } from '@naite/types';
 export const lessonPromptTemplate = {
   id: 'lesson-1.0',
   render: ({ language, topic, verbForms }: LessonConfig) => `
-    Provide a cloze test for the language:
-    ###
-    ${language}.
-    ###
-    The cloze test should be about the topic:
-    ###
-    ${topic}.
-    ###
-    The cloze test should include following verb forms:
-    ###
+    Provide a cloze test for the language ${language}.
+    The gaps should be filled with following word types:
     ${verbForms.reduce((verbForm) => `${verbForm}, `, '')}.
-    ###
+    The sentences should be around the topic ${topic}.
+    The gaps of the cloze test should be represented by parentheses containing the number of the gap corresponding to the solution, for example like this for the first gap (1).
+    Provide the solution for the spots in this format: number of spot : solution, for example: 1 : apple
     Write the cloze test in a modern tone.
-    Provide the solution for the cloze test seperated from the cloze test itself by a row of '#'
+    Mark the cloze test by writing "Cloze Test:" in the line before and mark the solution by writing "Solution:" in the line before.
    `,
 } satisfies PromptTemplate<LessonConfig>;
