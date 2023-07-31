adb457da91bc897b1c320e76394c47de1bee1b06
robinsadeghpour <r.sadegh.f@gmail.com>
2023-07-31 22:50:29 +0200

    feat: dialoq icon svg and favicon

--- apps/frontend/src/components/Navigation/Logo.tsx
+++ apps/frontend/src/components/Navigation/Logo.tsx
@@ -1,10 +1,10 @@
 import { Flex, Icon } from '@chakra-ui/react';
-import { ReactComponent as TLVIcon } from '../../../public/tlv-icon.svg';
+import { ReactComponent as DialoqIcon } from '../../../public/dialoq-icon.svg';
 
 const Logo = (): JSX.Element => {
   return (
     <Flex alignItems="center" height="24" paddingX="4" justifyContent="center">
-      <Icon as={TLVIcon} boxSize="12" />
+      <Icon as={DialoqIcon} boxSize="12" />
     </Flex>
   );
 };
