1257f6f790f301fd54e8141317544e7a97effe72
robinsadeghpour <r.sadegh.f@gmail.com>
2023-08-01 15:34:49 +0200

    feat: remove theme toggle from usermenu => moved to settings

--- apps/frontend/src/components/Navigation/UserMenu.tsx
+++ apps/frontend/src/components/Navigation/UserMenu.tsx
@@ -1,23 +1,15 @@
-import { MenuItem, MenuList, useColorMode } from '@chakra-ui/react';
+import { MenuItem, MenuList } from '@chakra-ui/react';
 import { AiOutlineLogout, AiOutlineUser } from 'react-icons/ai';
-import { FaMoon, FaSun } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../../services/auth.service';
 
 export const UserMenu = (): JSX.Element => {
-  const { colorMode, toggleColorMode } = useColorMode();
   const { mutate: logout } = useLogout();
 
   return (
     <MenuList>
       <MenuItem icon={<AiOutlineUser />} as={Link} to="/profile">
         Profile
-      </MenuItem>
-      <MenuItem
-        icon={colorMode === 'dark' ? <FaSun /> : <FaMoon />}
-        onClick={toggleColorMode}
-      >
-        {colorMode === 'dark' ? 'Light Mode' : 'Dark Mode'}
       </MenuItem>
       <MenuItem
         onClick={() =>
